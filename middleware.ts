fa939525ad529bc090d6ff947e345dd696969a06
ARLU <arludave23@gmail.com>
2024-03-25 14:44:20 +0800

    /analytics

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
-  publicRoutes: ["/", "/dashboard", "analytics"],
+  publicRoutes: ["/", "/dashboard", "/analytics"],
   // Prevent the specified routes from accessing
   // authentication information:
   // ignoredRoutes: ['/no-auth-in-this-route'],
