74a7ddc798bfa94b5bd03fe17900090f76edbd12
ARLU <arludave23@gmail.com>
2024-04-07 23:33:37 +0800

    hays

--- components/atoms/DeviceOffline.tsx
+++ components/atoms/DeviceOffline.tsx
@@ -17,9 +17,11 @@ const IsDeviceOffline = () => {
         deviceOnline ? "hidden" : ""
       } absolute top-0 left-0 right-0 bottom-0 w-full h-full backdrop-blur-sm grid place-items-center`}
     >
-      <div className="bg-primary text-center border rounded-lg px-10 py-6 animate-pulse duration-1000 ">
+      <div className="bg-primary text-center border rounded-lg px-10 py-6  ">
         <h1 className="text-primary pb-2">
-          <span className="font-bold">Waiting</span>
+          <span className="font-bold animate-pulse duration-300">
+            Waiting...
+          </span>
         </h1>
         <h5 className="font-normal text-red-400">Device Offline</h5>
       </div>
