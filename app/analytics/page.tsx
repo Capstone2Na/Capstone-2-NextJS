18c239c87d3f11e02701a651cecad15b2efc8b08
ARLU <arludave23@gmail.com>
2024-04-09 15:47:15 +0800

    edit

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import Loading from "@/components/atoms/Loading";
+import { Grid } from "@mui/material";
 import Charts from "@/components/atoms/Charts";
 
 const Analytics = () => {
   return (
-    <div className="section h-full overflow-x-hidden overflow-y-auto text-secondary">
-      <div className="content flex-col justify-between items-center h-full py-4 ">
-        {/* <Charts xAxis={} series={} width={800} height={600}/> */}
-        Belat
-      </div>
-    </div>
+    <Grid container spacing={2} className="h-full mx-4">
+      <Grid item xs={12}>
+        summary
+      </Grid>
+      <Grid item xs={12}>
+        {/* <Charts /> */}
+      </Grid>
+      <Grid item xs={12}>
+        charts 2
+      </Grid>
+      <Grid item xs={12}>
+        charts 3
+      </Grid>
+    </Grid>
   );
 };
 
 export default dynamic(() => Promise.resolve(Analytics), {
   ssr: true,
-  loading: () => <Loading />,
 });
--- app/page.tsx
+++ app/page.tsx
@@ -2,44 +2,40 @@ import Image from "next/image";
 import Link from "next/link";
 import { appLabels } from "./appLabels";
 import dynamic from "next/dynamic";
-// import Loading from "@/components/atoms/Loading";
 import { Button } from "@mui/material";
 import { AppLogo } from "@/components/molecules/PageUpper";
 
 function Home() {
   return (
     <>
-      <main className="w-full h-full overflow-x-hidden overflow-y-auto">
-        <div className="h-full max-h-lvh relative items-center w-full   ">
-          <Image
-            fill={true}
-            quality={100}
-            src={"/HydroSync/Image1.jpg"}
-            alt="HydroSync Lading Img"
-            className=" object-cover  w-screen relative -z-50 filter brightness-50"
-          />
-          <div className=" absolute top-0 h-full w-full flex flex-col items-center bg-gradient-to-b from-transparent to-black opacity-90">
-            <AppLogo
-              className={"mx-auto mt-[30vh] text-5xl text-primary lg:text-6xl"}
-            />
-            <p className="text-tertiary text-xs md:text-sm lg:text-xl mt-2 md:mt-3">
-              BSCpE 4 Capstone Project{" "}
-            </p>
-            <Link
-              href={"/dashboard"}
-              className="mt-2 animate-pulse duration-[2s] hover:animate-none "
-            >
-              <Button
-                variant="outlined"
-                className=" text-nowrap w-min bg-transparent hover:bg-buttonHover hover:text-secondary font-bold lg:text-lg lg:px-4 lg:py-2 "
-              >
-                Get Started
-              </Button>
-            </Link>
-          </div>
-        </div>
+      <Image
+        fill={true}
+        quality={100}
+        src={"/HydroSync/Image1.jpg"}
+        alt="HydroSync Lading Img"
+        className=" object-cover  w-screen relative -z-50 filter brightness-50"
+      />
+      <div className=" absolute top-0 h-full w-full flex flex-col items-center bg-gradient-to-b from-transparent to-black opacity-90">
+        <AppLogo
+          className={"mx-auto mt-[30vh] text-5xl text-primary lg:text-6xl"}
+        />
+        <p className="text-tertiary text-xs md:text-sm lg:text-xl mt-2 md:mt-3">
+          BSCpE 4 Capstone Project{" "}
+        </p>
+        <Link
+          href={"/dashboard"}
+          className="mt-2 animate-pulse duration-[2s] hover:animate-none "
+        >
+          <Button
+            variant="outlined"
+            className=" text-nowrap w-min bg-transparent hover:bg-buttonHover hover:text-secondary font-bold lg:text-lg lg:px-4 lg:py-2 "
+          >
+            Get Started
+          </Button>
+        </Link>
+      </div>
 
-        {/* <div className="content items-center overflow-x-hidden overflow-y-auto mx-auto h-full max-h-full text-secondary">
+      {/* <div className="content items-center overflow-x-hidden overflow-y-auto mx-auto h-full max-h-full text-secondary">
           <div className="flex flex-col pt-6">
             <h1 className="text-3xl font-semibold mb-2 text-center">
               Welcome to{" "}
@@ -67,7 +63,6 @@ function Home() {
             </div>
           </div>
         </div> */}
-      </main>
     </>
   );
 }
