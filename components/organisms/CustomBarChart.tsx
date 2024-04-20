6f4340298a707ccea2ae42859832067dae11b084
ARLU <arludave23@gmail.com>
2024-04-20 15:37:53 +0800

    cc

--- components/organisms/CustomBarChart.tsx
+++ components/organisms/CustomBarChart.tsx
@@ -81,10 +81,10 @@ export default function CustomBarchart() {
       </h1>
       <div className="flex flex-col items-center lg:flex-row lg:pl-8 lg:pr-4 gap-2">
         <div className="lg:w-[20%] flex flex-col gap-6 mt-6">
-          <h4 className="text-nowrap lg:text-wrap text-xl lg:text-4xl">
+          {/* <h4 className="text-nowrap lg:text-wrap text-xl lg:text-4xl">
             Total in Liters:{" "}
             <span className=" text-2xl lg:text-5xl">{value}</span>
-          </h4>
+          </h4> */}
 
           <SelectTimeFrame />
         </div>
